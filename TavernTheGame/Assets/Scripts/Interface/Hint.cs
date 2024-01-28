79a90b9c894300ec969e0f08e2220cf592139344
Lul1ch <84387626+Lul1ch@users.noreply.github.com>
2024-01-28 20:24:55 +0300

    Hint script update

--- TavernTheGame/Assets/Scripts/Interface/Hint.cs
+++ TavernTheGame/Assets/Scripts/Interface/Hint.cs
@@ -10,14 +10,15 @@ public class Hint : MonoBehaviour
         get { return _hintLifeTime; }
     }
     public enum EventType {
-        NotEnoughMoney, NotEnoughProducts, NotEnoughBonuses, InappropriateTimeForService
+        NotEnoughMoney, NotEnoughProducts, NotEnoughBonuses, InappropriateTimeForService, BonusAlreadyBought
     }
 
     Dictionary<EventType, string> hintsForPlayer = new Dictionary<EventType, string>() {
         [EventType.NotEnoughMoney] = "Недостаточно денег для покупки этого продукта. Нужно ещё ",
         [EventType.NotEnoughProducts] = "Недостаточно продуктов для готовки данного блюда. Нужно ещё ",
         [EventType.NotEnoughBonuses] = "Недостаточно очков репутации для покупки этого бонуса. Нужно ещё ",
-        [EventType.InappropriateTimeForService] = "Сейчас не требуется выдавать еду клиенту."
+        [EventType.InappropriateTimeForService] = "Сейчас не требуется выдавать еду клиенту.",
+        [EventType.BonusAlreadyBought] = "Бонус уже куплен."
     };
 
     public void ShowHint(EventType eventType, string additionalString = "") {
